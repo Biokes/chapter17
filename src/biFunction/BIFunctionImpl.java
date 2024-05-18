14a9e9190b046ee5b0e5754a3977ff027b65e4eb
Biokes <106444765+Biokes@users.noreply.github.com>
2024-05-18 14:57:38 +0100

    create bifunction implementation

--- src/biFunction/BIFunctionImpl.java
+++ src/biFunction/BIFunctionImpl.java
@@ -1,7 +1,10 @@
 package biFunction;
 
+import java.util.function.BiFunction;
+
 public class BIFunctionImpl {
     public static void main(String[] args) {
-        BiFunction<>
+        BiFunction<Integer,Integer, Integer> biFunction = (num1, num2)->num1*num2;
+        System.out.println("biFunction.apply(12,32) = " + biFunction.apply(12, 32));
     }
 }
