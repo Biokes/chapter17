3be7d1f3a28602e1faf44a3787e57d7b5360251d
Biokes <106444765+Biokes@users.noreply.github.com>
2024-05-18 15:59:49 +0100

    supplier implementation

--- src/booleanSupplier/BooleanSupplierOmpl.java
+++ src/booleanSupplier/BooleanSupplierOmpl.java
@@ -1,2 +1,11 @@
-package booleanSupplier;public class BooleanSupplierOmpl {
+package booleanSupplier;
+
+import java.util.function.BooleanSupplier;
+
+public class BooleanSupplierOmpl {
+    public static void main(String[] args) {
+        BooleanSupplier output = ()->true;
+        System.out.println(output.getAsBoolean());
+
+    }
 }
