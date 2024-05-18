5cf028409cacba148d45c22437848cc2ca33a8fe
Biokes <106444765+Biokes@users.noreply.github.com>
2024-05-18 15:29:53 +0100

    bi-predicate implementation

--- src/unaryOperator/UnaryOperatorImpl.java
+++ src/unaryOperator/UnaryOperatorImpl.java
@@ -1,2 +1,10 @@
-package unaryOperator;public class UnaryOperatorImpl {
+package unaryOperator;
+
+import java.util.function.UnaryOperator;
+
+public class UnaryOperatorImpl {
+    public static void main(String[] args) {
+        UnaryOperator<String> input = (name)->name +" is a man";
+        System.out.println("input.apply(\"abbey\") = " + input.apply("abbey"));
+    }
 }
