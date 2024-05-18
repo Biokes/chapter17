115f8612592b6e9bb97cb0ca0a16f308d2e86910
Biokes <106444765+Biokes@users.noreply.github.com>
2024-05-18 15:33:01 +0100

    bi-predicate implementation

--- src/binaryOperator/BinaryOperatorImpl.java
+++ src/binaryOperator/BinaryOperatorImpl.java
@@ -1,2 +1,10 @@
-package binaryOperator;public class BinaryOperatorImpl {
+package binaryOperator;
+
+import java.util.function.BinaryOperator;
+
+public class BinaryOperatorImpl {
+    public static void main(String[] args) {
+        BinaryOperator<String> output = (micheal, melody)->micheal + " was whooped by " + melody;
+        System.out.println(output.apply("mikel", "melody"));
+    }
 }
