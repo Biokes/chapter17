eda8cc5de21bf45d33aa46cc22512ef0ac8ed128
Biokes <106444765+Biokes@users.noreply.github.com>
2024-05-18 15:23:01 +0100

    predicate impl

--- src/biconsumer/BiConsumerImpl.java
+++ src/biconsumer/BiConsumerImpl.java
@@ -1,2 +1,11 @@
-package biconsumer;public class BiConsumerImpl {
+package biconsumer;
+
+import java.util.function.BiConsumer;
+
+public class BiConsumerImpl {
+    public static void main(String[] args) {
+        BiConsumer<Integer,Integer> output = (value,name)->value*=name;
+        output.accept(12,5);
+        System.out.println("output = " + output);
+    }
 }
