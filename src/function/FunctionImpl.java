b072d20da458c7cbfdb49f995735579a912058a7
Biokes <106444765+Biokes@users.noreply.github.com>
2024-05-18 15:06:37 +0100

    function impl

--- src/function/FunctionImpl.java
+++ src/function/FunctionImpl.java
@@ -5,7 +5,7 @@ import java.util.function.Function;
 public class FunctionImpl {
     public static void main(String[] args) {
         Function<String ,String> output = (name)->name+ "is a boy";
-        System.out.println(output.apply("abbey"));add .
+        System.out.println(output.apply("abbey"));
 
     }
 }
