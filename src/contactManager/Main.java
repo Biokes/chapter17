30ddfb980f0bb9c23ce394012562a96a7a471ff9
Biokes <106444765+Biokes@users.noreply.github.com>
2024-05-21 14:48:40 +0100

    add a testing

--- src/contactManager/Main.java
+++ src/contactManager/Main.java
@@ -7,18 +7,21 @@ public class Main {
     public static void main(String[] args) {
         Repo repo = new Repo();
         Contact contact = new Contact();
-        contact.setName("abbey");
+        contact.setName("zaddi");
         contact.setEmail("email123@gmail.com");
         contact.setNumber("09009980");
         repo.addContact(contact);
-        repo.addContact(contact);
-        contact.setName("biokes");
-        repo.addContact(contact);
-        contact.setName("biokes");
-        repo.addContact(contact);
-        contact.setName("abb");
-        repo.addContact(contact);
-        repo.findAllContact().stream().forEach(contact1 -> System.out.println(contact1.toString()));
+        Contact contact1 = new Contact();
+        contact1.setName("abbey");
+        contact1.setEmail("email@gmail.com");
+        contact1.setNumber("09009980");
+        repo.addContact(contact1);
+        Contact contact2 = new Contact();
+        contact2.setName("biokes123");
+        contact2.setNumber("78907645447");
+        contact2.setEmail("mail@gmail.com");
+        repo.addContact(contact2);
+        repo.findAllContact().forEach(System.out::println);
 
     }
 }
